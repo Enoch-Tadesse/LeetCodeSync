7d1b55995e9ab66757547d6b79c06d9d20dc8f3d
Henok <henitadesse97@gmail.com>
2024-07-17 16:15:55 +0300

    Time: 17 ms (54.51%) | Memory: 11.6 MB (21.75%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -4,9 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums)):
-            for j in range(1 , len(nums)):
-                if nums[j] < nums[j-1]:
-                    nums[j] , nums[j-1] = nums[j-1] , nums[j]
-            
-        
+        count = [0]*3
+        for num in nums:
+            count[num]+=1
+        j = 0
+        for i in range(len(count)):
+            while count[i] > 0:
+                nums[j] = i
+                j+=1
+                count[i]-=1
