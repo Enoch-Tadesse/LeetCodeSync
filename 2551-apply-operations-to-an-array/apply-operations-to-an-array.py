9c934c812e7766e40169ef5aabdb6d0e55c83c4a
Henok <henitadesse97@gmail.com>
2024-07-17 11:25:45 +0300

    Time: 25 ms (87.76%) | Memory: 12 MB (16.88%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -9,12 +9,10 @@ class Solution(object):
             if nums[i] == nums[i+1]:
                 nums[i]*=2
                 nums[i+1] = 0
-        zeros = []
-        for num in nums:
-            if num == 0:
-                zeros.append(num)
-            else:
-                result.append(num)
-        result.extend(zeros)
-        return result
+        ptr = 0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[i] , nums[ptr] = nums[ptr] , nums[i]
+                ptr+=1
+        return nums
         
