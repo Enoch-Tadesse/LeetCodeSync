1985d4e3e3ea6e6f43cbe97c5fa977ac57aa24e6
Henok <henitadesse97@gmail.com>
2024-07-17 14:59:15 +0300

    Time: 119 ms (51.12%) | Memory: 13.1 MB (20.69%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+        g.sort()
+        s.sort()
+        i = j = 0
+        while j < len(s) and i < len(g):
+            if g[i] <= s[j]:
+                i , j = i+1 , j+1
+            elif s[j] < g[i]:
+                j+=1
+            
+        return i
