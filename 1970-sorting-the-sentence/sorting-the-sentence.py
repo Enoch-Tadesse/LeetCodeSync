a3cd736c90691a35ba4de0128ec942d96ed52335
Henok <henitadesse97@gmail.com>
2024-07-17 08:22:14 +0300

    Time: 13 ms (55.97%) | Memory: 11.8 MB (17.80%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.py
+++ 1970-sorting-the-sentence/sorting-the-sentence.py
@@ -4,9 +4,8 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        output = ""
         s_list = [(item[-1], item[0:-1]) for item in s.split(" ")]
-        s_list.sort(key = lambda x : x[0])
+        s_list.sort()
         s_list2 = [item[1] for item in s_list]
         return (" ").join(s_list2)
         
