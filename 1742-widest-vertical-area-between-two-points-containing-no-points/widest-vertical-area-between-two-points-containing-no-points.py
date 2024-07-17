13bbb3834ba87d85102dc8165424542e02ee0b01
Henok <henitadesse97@gmail.com>
2024-07-17 15:27:44 +0300

    Time: 671 ms (35.86%) | Memory: 57.7 MB (88.92%) - LeetSync

--- 1742-widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.py
+++ 1742-widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type points: List[List[int]]
         :rtype: int
         """
-        points.sort()
+        points.sort(key = lambda x : x[0])
         max_width = 0
         for i in range(len(points)-1, 0, -1):
             if points[i][0] - points[i-1][0] > max_width:
