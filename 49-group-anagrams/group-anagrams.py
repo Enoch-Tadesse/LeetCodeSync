2eee964d26a124218951cc7d06968a4d978f6017
Henok <henitadesse97@gmail.com>
2024-07-19 17:20:11 +0300

    Time: 72 ms (60.16%) | Memory: 15.6 MB (73.61%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -5,12 +5,10 @@ class Solution(object):
         :rtype: List[List[str]]
         """
         the_hash = {}
-        output = []
         for s in strs:
-            sorted_s = sorted(s)
-            sorted_s = ("").join(sorted_s)
+            sorted_s = ("").join(sorted(s))
             if sorted_s not in the_hash:
                 the_hash[sorted_s] = [s]
             else:
                 the_hash[sorted_s].append(s)
-        return [val for val in the_hash.values()]
+        return list(the_hash.values())
