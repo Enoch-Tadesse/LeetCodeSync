ea1ac259932bcc3d02401df58544f30128fc92ac
Henok <henitadesse97@gmail.com>
2024-07-19 18:15:04 +0300

    Time: 178 ms (59.04%) | Memory: 12.9 MB (15.14%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -5,15 +5,15 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        def binary(nums , l , r, target):
-            if l > r:
-                return -1
-            mid = (l+r)//2
+        i = 0
+        j = len(nums)-1
+        while i <= j:
+            mid = i + (j-i)//2
             if nums[mid] == target:
                 return mid
             elif nums[mid] > target:
-                return binary(nums, l, mid-1, target)
+                j = mid - 1
             else:
-                return binary(nums, mid+1, r, target)
-        return binary(nums, 0 , len(nums)-1, target)
+                i = mid + 1
+        return -1
         
