5c27d54007ed72fdca83a09b8080b8be2efffc16
Henok <henitadesse97@gmail.com>
2024-07-26 17:31:21 +0300

    Time: 183 ms (79.42%) | Memory: 22.5 MB (15.74%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -5,15 +5,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        result = float('inf')
-        r = 0
         l = 0
-        sum = 0
+        r = 0
+        result = float('inf')
+        temp = 0
         while r < len(nums):
-            sum += nums[r]
-            while sum >= target:
-                result = min(result, r - l + 1)
-                sum -= nums[l]
+            temp += nums[r]
+            while temp >= target:
+                temp -= nums[l]
+                result = min(result, r-l+1)
                 l+=1
             r+=1
-        return 0 if result == float('inf') else result
+        return result if result != float('inf') else 0
