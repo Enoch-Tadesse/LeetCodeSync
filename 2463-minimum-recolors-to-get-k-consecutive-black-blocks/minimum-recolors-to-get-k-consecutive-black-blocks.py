f737677b431eb09aabb07f0f5fab7581751c209b
Henok <henitadesse97@gmail.com>
2024-07-26 20:51:27 +0300

    Time: 12 ms (84.35%) | Memory: 11.6 MB (45.22%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -5,18 +5,19 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        result = []
-        i = 0
-        j = k
-        while i  <= len(blocks)-k:
-            whites = 0
-            for block in blocks[i:j]:
-                if block == "W":
-                    whites+=1
-            j+=1
-            i+=1
-            result.append(whites)
-        return min(result)
+        min_p = k
+        curr = 0
+        for i in range(k):
+            if blocks[i] == 'W':
+                curr+=1
+        min_p = min(min_p, curr)
+        for j in range(k,len(blocks)):
+            if blocks[j] == 'W':
+                curr+=1
+            if blocks[j-k] == 'W':
+                curr-=1
+            min_p = min(min_p, curr)
+        return min_p
 
 
         
