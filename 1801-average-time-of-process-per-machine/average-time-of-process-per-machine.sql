0b7a7d63455574defe7710feab9e69dd33cb0a22
Henok <henitadesse97@gmail.com>
2024-07-30 18:39:06 +0300

    Time: 251 ms (24.17%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT t.machine_id, ROUND(AVG(diff),3) AS processing_time 
+FROM (
+	SELECT a1.machine_id as machine_id , round(a1.timestamp - a2.timestamp, 3) AS diff
+	FROM Activity a1, Activity a2
+	WHERE (a1.machine_id, a1.process_id ) = (a2.machine_id, a2.process_id) and (a1.activity_type, a2.activity_type) = ('end', 'start')
+) t
+GROUP BY (t.machine_id)
