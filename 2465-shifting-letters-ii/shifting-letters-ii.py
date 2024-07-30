be1d46db0771852610fb17f0d5aaef59530cbe8b
Henok <henitadesse97@gmail.com>
2024-07-30 21:38:31 +0300

    Time: 1014 ms (56.51%) | Memory: 42 MB (32.70%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.py
+++ 2465-shifting-letters-ii/shifting-letters-ii.py
@@ -1,22 +1,12 @@
-class Solution(object):
-    def shiftingLetters(self, s, shifts):
-        """
-        :type s: str
-        :type shifts: List[List[int]]
-        :rtype: str
-        """
-        result = ""
-        update = [0] * (len(s) + 1)
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        output = ""
+        update = [0] *(len(s)+1)
         for left, right, dir in shifts:
-            if dir == 0:
-                update[left] += -1
-                update[right+1] += 1
-            else:
-                update[left] += 1
-                update[right+1] += -1
-        for k in range(1, len(update)):
-            update[k] += update[k-1]
+            update[left] += 1 if dir == 1 else -1
+            update[right + 1] +=1 if dir == 0 else -1
+        for i in range(1, len(update)):
+            update[i] += update[i-1]
         for i in range(len(s)):
-            result+= chr((ord(s[i]) - ord('a') + update[i]) % 26 + ord('a'))
-        return result
-            
+            output+=chr(((ord(s[i])-ord('a') + update[i]) %26)+ord('a'))
+        return output
