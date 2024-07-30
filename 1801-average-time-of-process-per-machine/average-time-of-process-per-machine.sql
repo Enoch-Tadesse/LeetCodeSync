1ad575602840cd031caaf6986f4dea7969071072
Henok <henitadesse97@gmail.com>
2024-07-30 18:51:54 +0300

    Time: 273 ms (19.05%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -2,7 +2,7 @@
 SELECT t.machine_id, ROUND(AVG(diff),3) AS processing_time 
 FROM (
 	SELECT a1.machine_id as machine_id , round(a1.timestamp - a2.timestamp, 3) AS diff
-	FROM Activity a1, Activity a2
-	WHERE (a1.machine_id, a1.process_id ) = (a2.machine_id, a2.process_id) and (a1.activity_type, a2.activity_type) = ('end', 'start')
+	FROM Activity a1
+    JOIN Activity a2 ON (a1.machine_id, a1.process_id ) = (a2.machine_id, a2.process_id) AND (a1.activity_type, a2.activity_type) = ('end', 'start')
 ) t
 GROUP BY (t.machine_id)
