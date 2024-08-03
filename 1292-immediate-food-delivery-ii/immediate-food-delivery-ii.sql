55951c2dc404cdbf631d5abb23dd4f210e83c26a
Henok <henitadesse97@gmail.com>
2024-08-03 21:17:35 +0300

    Time: 590 ms (53.09%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT round(count(case when t.immediate = 1 then 1 end)/count(t.customer_id) * 100,2) AS immediate_percentage
+SELECT round(avg(t.immediate) * 100,2) AS immediate_percentage
 FROM (SELECT customer_id, MIN(order_date) = MIN(customer_pref_delivery_date) as immediate
 		FROM delivery
 		GROUP BY customer_id) t
