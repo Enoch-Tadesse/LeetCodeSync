190979bca735f6018696aa4c52c744d6631daca8
Henok <henitadesse97@gmail.com>
2024-08-05 21:46:53 +0300

    Time: 660 ms (69.19%) | Memory: 31 MB (7.70%) - LeetSync

---
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.py
@@ -0,0 +1,13 @@
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        run = [0] * (n + 1)
+        output = []
+        for first, last, seats in bookings:
+            run[first-1] += seats
+            run[last] -= seats
+        print(run)
+        runSum = 0
+        for i in range(len(run)-1):
+            runSum += run[i]
+            output.append(runSum)
+        return output
