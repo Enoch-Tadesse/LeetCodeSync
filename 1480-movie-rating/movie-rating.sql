46ee9d89fe3a055d50857eead5ebe18c7e47c15a
Henok <henitadesse97@gmail.com>
2024-08-06 21:29:47 +0300

    Time: 1087 ms (70.30%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -4,19 +4,16 @@
 FROM movierating mr
   JOIN users u
     ON mr.user_id = u.user_id
-GROUP BY mr.user_id,
-         u.name
-ORDER BY COUNT(movie_id) DESC, u.name
+GROUP BY u.name
+ORDER BY COUNT(*) DESC, u.name
 LIMIT 1)
 
 union all
 (SELECT
   m.title 
-FROM (
-  SELECT *
-  FROM movierating
-  WHERE created_at between '2020-02-01' and '2020-02-28'
-) t
-JOIN movies m ON t.movie_id = m.movie_id
-ORDER BY avg(t.rating) OVER (PARTITION BY t.movie_id) desc, m.title
+FROM movierating mr
+JOIN movies m ON mr.movie_id = m.movie_id
+WHERE mr.created_at between '2020-02-01' and '2020-02-28'
+group by m.title
+ORDER BY avg(mr.rating) desc, m.title
 LIMIT 1);
