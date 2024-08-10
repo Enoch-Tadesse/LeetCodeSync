0f3cfe4ffba37bdbbb65ec7cddb130b71b89a877
Henok <henitadesse97@gmail.com>
2024-08-10 16:53:13 +0300

    Time: 42 ms (15.37%) | Memory: 16.6 MB (45.32%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,21 +1,17 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        parent = {
-            "]" : "[",
-            ")" : "(",
-            "}" : "{"
-        }
+class Solution:
+    def isValid(self, s: str) -> bool:
         stack = []
+        myDic = {
+            ']' : '[',
+            '}' : '{',
+            ')' : '('
+        }
         for item in s:
-            if item not in parent:
+            if item not in myDic:
                 stack.append(item)
-                continue
-            if len(stack) > 0 and (parent[item] == stack[-1]):
+            elif len(stack) > 0 and stack[-1] == myDic[item]:
                 stack.pop()
-            else:
-                return False
-        return len(stack) == 0
+            else: return False
+        if len(stack) == 0:
+            return True
+        return False
