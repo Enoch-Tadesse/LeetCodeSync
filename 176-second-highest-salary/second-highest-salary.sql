ff50df2472c392fac278e9b112b7af0d24adf31e
Henok <henitadesse97@gmail.com>
2024-08-11 18:57:33 +0300

    Time: 1543 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,5 +1,4 @@
-# Write your MySQL query statement below
-select coalesce ((select distinct salary from employee
+select(select distinct salary from employee
 order by salary desc
 limit 1
-offset 1), null) as SecondHighestSalary
+offset 1) as SecondHighestSalary
