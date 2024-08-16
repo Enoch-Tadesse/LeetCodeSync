64e0bb94b88975a4358e15a6917aff2934f95aa0
Henok <henitadesse97@gmail.com>
2024-08-16 20:48:19 +0300

    Time: 449 ms (35.97%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -1,10 +1,9 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT DETERMINISTIC
 BEGIN
   RETURN (
-    select t.salary from(SELECT DENSE_RANK() over(order by salary desc) as rnk,
+    select distinct t.salary from(SELECT DENSE_RANK() over(order by salary desc) as rnk,
                     e.salary
     FROM Employee e ) t
     where t.rnk = N
-    limit 1
   );
 END
