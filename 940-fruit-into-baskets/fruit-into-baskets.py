d222c8122b91dc1157cd7a4d7bd5d6b6e46b80cc
Henok <henitadesse97@gmail.com>
2024-08-17 08:09:28 +0300

    Time: 604 ms (89.22%) | Memory: 23.9 MB (85.36%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -4,17 +4,13 @@ class Solution:
         l = 0
         max_idx = defaultdict(int)
         for r in range(len(fruits)):
-            if len(max_idx.keys()) < 2 or fruits[r] in max_idx:
+            if len(max_idx) < 2 or fruits[r] in max_idx:
                 max_idx[fruits[r]] = r
                 result = max(result, r-l+1)
                 continue
-            print(max_idx)
             result = max(result, r-l)
             l_idx = sum(list(max_idx.keys())) - fruits[r-1]
             l = max_idx[l_idx] + 1
             max_idx[fruits[r]] = r
             del max_idx[l_idx]
-            print(max_idx)
-        if result == -1:
-            return len(fruits)
         return result
