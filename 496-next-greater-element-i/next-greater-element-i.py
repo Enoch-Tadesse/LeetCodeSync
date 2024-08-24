1ac08920ec549771b987b3084221fa9c4599afd2
Henok <henitadesse97@gmail.com>
2024-08-24 07:47:44 +0300

    Time: 55 ms (35.37%) | Memory: 16.9 MB (25.07%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,14 +1,17 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         stack = []
-        result = deque()
+        # result = deque()
+        result = defaultdict(int)
 
-        for i in range(len(nums2)-1, -1,-1):
+        # for i in range(len(nums2)-1, -1,-1):
+        for i in range(len(nums2)-1, -1, -1):
             while stack and stack[-1] <= nums2[i]:
                 stack.pop()
             if stack:
-                result.appendleft(stack[-1])
-            else:
-                result.appendleft(-1)
+                result[nums2[i]] = stack[-1]
+            else: result[nums2[i]] = -1
             stack.append(nums2[i])
-        return [result[nums2.index(item)] for item in nums1]
+        return [result[item] for item in nums1]
+        print(result)
+        # return [result[nums2.index(item)] for item in nums1]
