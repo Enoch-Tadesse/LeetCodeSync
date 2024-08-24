49bccd1f5f29b3b5e35191c40f17f52be09735ed
Henok <henitadesse97@gmail.com>
2024-08-24 09:42:08 +0300

    Time: 53 ms (28.56%) | Memory: 16.7 MB (7.08%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -2,16 +2,8 @@ class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
         disc = defaultdict(int)
         stack = []
-        output = []
-        for i in range(len(prices)-1, -1, -1):
-            while stack and stack[-1] > prices[i]:
-                stack.pop()
-            if stack:
-                disc[i] = stack[-1]
-            else:
-                disc[i] = 0
-            stack.append(prices[i])
-        return [prices[i] - disc[i] for i in range(len(prices))]
-        # for i in range(len(prices)):
-        #     output.append(prices[i] - disc[i])
-        # return output
+        for i in range(len(prices)):
+            while stack and prices[stack[-1]] >= prices[i]:
+                prices[stack.pop()] -= prices[i]
+            stack.append(i)
+        return prices
