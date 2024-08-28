7bb193c7fe39d4825017d5fa02085aab65c87865
Henok <henitadesse97@gmail.com>
2024-08-28 19:25:01 +0300

    Time: 86 ms (14.03%) | Memory: 16.4 MB (99.30%) - LeetSync

--- 326-power-of-three/power-of-three.py
+++ 326-power-of-three/power-of-three.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        if type(n) == 'float' or n < 1:
+        if n < 1:
             return False
         if n == 1:
             return True
