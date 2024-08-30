f53ead409e76f85fdffa957b61aa6af4d9cefb71
Henok <henitadesse97@gmail.com>
2024-08-30 09:03:06 +0300

    Time: 38 ms (29.50%) | Memory: 16.5 MB (14.98%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,10 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        @lru_cache
+        self.memo = defaultdict()
         def helper(n ,k):
+            if self.memo.get(k,None):
+                return self.memo[k]
             if k == n:
                 return 1
             if k > n:
                 return 0
-            return helper(n, k+1) + helper(n, k+2)
+            self.memo[k] = helper(n, k+1) + helper(n, k+2)
+            return self.memo[k]
         return helper(n, 0)
