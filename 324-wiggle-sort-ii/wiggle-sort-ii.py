4cc88597c36a6291de4b79b57e23e21d83871ca4
Henok <henitadesse97@gmail.com>
2024-09-03 11:14:28 +0300

    Time: 120 ms (84.40%) | Memory: 19.4 MB (44.47%) - LeetSync

--- 324-wiggle-sort-ii/wiggle-sort-ii.py
+++ 324-wiggle-sort-ii/wiggle-sort-ii.py
@@ -5,7 +5,7 @@ class Solution:
         """
         nums.sort()
         mid = (len(nums)-1)//2
-        nums[::2] , nums[1::2] =  nums[mid::-1], nums[:mid:-1]
+        nums[::2] , nums[1::2] = nums[mid::-1],nums[:mid:-1]
         
 
         
