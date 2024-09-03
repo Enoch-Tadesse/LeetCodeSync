ac1fca1998859cac61532058f76da394c43da826
Henok <henitadesse97@gmail.com>
2024-09-03 16:22:51 +0300

    Time: 236 ms (30.16%) | Memory: 19.5 MB (56.70%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -3,11 +3,12 @@ class Solution:
         count = 0
         preSum = 0
         fre = defaultdict(int)
-        fre[0] = 1
         for num in nums:
             preSum += num
             if (preSum-k) in fre:
                 count+=fre[preSum-k]
+            if preSum - k == 0:
+                count+=1
             fre[preSum]+=1
         return count
 
