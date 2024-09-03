0c11284440fb89935d94ce607888911a245431f8
Henok <henitadesse97@gmail.com>
2024-09-03 19:23:11 +0300

    Time: 68 ms (40.22%) | Memory: 17.1 MB (61.38%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -2,15 +2,31 @@ class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
         operators = {
-                "+" : lambda x,y : x+y,
-                "-" : lambda x,y : x-y,
-                "*" : lambda x,y : x*y,
-                "/" : lambda x,y : int(x/y)
+            "+" : lambda x , y : x + y,
+            "-" : lambda x, y: x - y,
+            "*" : lambda x,y : x * y,
+            "/" : lambda x, y: int(x/y)
         }
         for token in tokens:
-            if token in operators:
-                a = int(stack.pop())
-                b = int(stack.pop())
-                stack.append(operators[token](b,a))
-            else: stack.append(token)
-        return int(stack[0])
+            if token not in operators:
+                stack.append(token)
+            else:
+                second = stack.pop()
+                first = stack.pop()
+                num = operators[token](int(first),int(second))
+                stack.append(num)
+        return int(stack[-1])
+        # stack = []
+        # operators = {
+        #         "+" : lambda x,y : x+y,
+        #         "-" : lambda x,y : x-y,
+        #         "*" : lambda x,y : x*y,
+        #         "/" : lambda x,y : int(x/y)
+        # }
+        # for token in tokens:
+        #     if token in operators:
+        #         a = int(stack.pop())
+        #         b = int(stack.pop())
+        #         stack.append(operators[token](b,a))
+        #     else: stack.append(token)
+        # return int(stack[0])
