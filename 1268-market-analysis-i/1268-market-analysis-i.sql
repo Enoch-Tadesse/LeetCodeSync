a299307a855f87e706264c2c3fcb778285588464
Henok <henitadesse97@gmail.com>
2024-09-10 21:05:45 +0300

    Time: 1641 ms (11.07%), Space: 0B (100%) - LeetHub

--- 1268-market-analysis-i/1268-market-analysis-i.sql
+++ 1268-market-analysis-i/1268-market-analysis-i.sql
@@ -1,9 +1,4 @@
-with cte as(select user_id , count(order_id) as 'orders_in_2019'
+select distinct u.user_id buyer_id, u.join_date,
+	count(case when year(o.order_date) = 2019 then 1 end) over(partition by u.user_id ) as orders_in_2019
 from users u
-join orders o on u.user_id = o.buyer_id
-where year(o.order_date) = 2019
-group by user_id)
-
-select u.user_id buyer_id, u.join_date, coalesce(cte.orders_in_2019,0) orders_in_2019
-from users u
-left join cte on u.user_id = cte.user_id;
+left join orders o on u.user_id = o.buyer_id;
