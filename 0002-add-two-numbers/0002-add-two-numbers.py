8bdc6a2143cbaeb01a0765a6fc87252d7b62250a
Henok <henitadesse97@gmail.com>
2024-09-15 10:33:30 +0300

    Time: 56 ms (48.13%), Space: 16.6 MB (74.51%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -10,30 +10,32 @@ class Solution:
         dummy = ListNode()
         output = dummy
         temp = 0
-        while left and right:
-            print(dummy.next)
-            print(temp)
-            curr = left.val + right.val + temp
+        while left or right:
+            val1 = left.val if left else 0
+            val2 = right.val if right else 0
+            curr = val1 + val2 + temp
             re = curr % 10
             temp = curr//10
-            left = left.next
-            right = right.next
+            if left:
+                left = left.next
+            if right:
+                right = right.next
             output.next = ListNode(val=re)
             output = output.next
-        while left:
-            curr = left.val + temp
-            re = curr % 10
-            temp = curr//10
-            left = left.next
-            output.next = ListNode(val=re)
-            output = output.next
-        while right:
-            curr = right.val + temp
-            re = curr % 10
-            temp = curr//10
-            right = right.next
-            output.next = ListNode(val=re)
-            output = output.next
+        # while left:
+        #     curr = left.val + temp
+        #     re = curr % 10
+        #     temp = curr//10
+        #     left = left.next
+        #     output.next = ListNode(val=re)
+        #     output = output.next
+        # while right:
+        #     curr = right.val + temp
+        #     re = curr % 10
+        #     temp = curr//10
+        #     right = right.next
+        #     output.next = ListNode(val=re)
+        #     output = output.next
         if temp:
             output.next = ListNode(val=temp)
         return dummy.next
