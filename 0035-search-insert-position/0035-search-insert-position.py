1fd02457cc648b696edb53eb0895281b1c2a999b
Henok <henitadesse97@gmail.com>
2024-10-11 17:32:41 +0300

    Time: 55 ms (18.37%), Space: 17.5 MB (5.78%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,18 +1,13 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        self.i = 0
-        self.j = len(nums)-1
-        
-        def binary():
-            if self.i > self.j:
-                return self.i
-            mid = (self.i + self.j)//2
+        def helper(i,j,target):
+            if i > j:
+                return j + 1
+            mid = (i + j)//2
             if target == nums[mid]:
                 return mid
             elif target < nums[mid]:
-                self.j = mid - 1
-                return binary()
+                return helper(i,mid-1,target)
             else:
-                self.i = mid + 1
-                return binary()
-        return binary()
+                return helper(mid+1,j,target)
+        return helper(0,len(nums)-1,target)
