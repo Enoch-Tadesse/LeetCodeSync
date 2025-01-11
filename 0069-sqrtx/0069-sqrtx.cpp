e2c5b909b594ca04f8b7d2418fb163089623b06d
Henok <henitadesse97@gmail.com>
2025-01-11 06:34:18 +0300

    Time: 211 ms (5.04%), Space: 283.5 MB (11.08%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        vector<int> sam; if(x<0){ return-1;}
+        for(unsigned i=0;i*i<=x;i++){
+            if(i*i>INT_MAX){return -1;}
+            sam.push_back(i);  }
+        return sam[sam.size()-1];  
+        
+    }
+};
