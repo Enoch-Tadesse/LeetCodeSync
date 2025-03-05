a1d2d18514f260e01309cbf6cf01d4a4a3bf2ab5
Henok <henitadesse97@gmail.com>
2025-03-05 22:11:07 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (92.15%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        count = 0
+        for log in logs:
+            if log == "../":
+                count = max(0, count-1)
+                continue
+            if log == "./":
+                continue
+            count += 1
+        return count
+            
