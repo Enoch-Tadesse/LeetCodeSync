a64b6e68c00f1d8151e1a65e5dfb52d4383d14e8
Henok <henitadesse97@gmail.com>
2025-03-12 12:46:35 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (69.11%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
@@ -1,14 +1,26 @@
-class Solution(object):
-    def maximumCount(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        pos = 0
-        neg = 0
-        for num in nums:
-            if num < 0:
-                neg+=1
-            elif num > 0:
-                pos+=1
-        return max(pos,neg)
+class Solution:
+    def maximumCount(self, nums: List[int]) -> int:
+        neg = self.negBin(nums)
+        pos = self.posBin(nums)
+        return max(neg , len(nums) - pos)
+    def negBin(self, nums):
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            mid = (r + l) // 2
+            if nums[mid] >= 0:
+                r = mid - 1
+            elif nums[mid] < 0:
+                l = mid + 1
+        return l
+    def posBin(self, nums):
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            mid = (r + l) // 2
+            if nums[mid] > 0:
+                r = mid - 1
+            elif nums[mid] <= 0:
+                l = mid + 1
+        return l
+
