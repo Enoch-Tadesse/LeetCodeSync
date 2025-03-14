2479c77fcbb3430bbf81ab4d2c196d22758a5912
Henok <henitadesse97@gmail.com>
2025-03-14 20:05:11 +0300

    Time: 79 ms (8.44%) | Memory: 22.8 MB (82.28%) - LeetSync

---
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return []
+        q = deque([root])
+        output = self.traverse(root)
+        depth = 0
+        while q:
+            k = len(q)
+            for i in range(k):
+                if q[i].right or q[i].left:
+                    q.append(q[i].left)
+                    q.append(q[i].right)
+            if depth & 1:
+                l , r = 0, k - 1
+                while l < r:
+                    q[l].val , q[r].val = q[r].val , q[l].val
+                    l , r = l + 1 , r - 1
+            for i in range(k):
+                q.popleft()
+            depth += 1
+        return output[0]
+    def traverse(self, root):
+        if not root:
+            return []
+        return [root, *self.traverse(root.left) , *self.traverse(root.right)]
