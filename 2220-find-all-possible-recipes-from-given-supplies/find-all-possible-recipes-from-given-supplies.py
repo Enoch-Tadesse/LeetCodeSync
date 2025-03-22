5c2bc51a7563ce2dcd6c7b858718b60c5a0453de
Henok <henitadesse97@gmail.com>
2025-03-22 18:48:17 +0300

    Time: 83 ms (53.16%) | Memory: 19.8 MB (58.27%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.py
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.py
@@ -1,35 +1,18 @@
 class Solution:
-    def findAllRecipes(
-        self,
-        recipes: list[str],
-        ingredients: list[list[str]],
-        supplies: list[str],
-    ) -> list[str]:
-        # Track available ingredients and recipes
-        available = set(supplies)
-
-        # Queue to process recipe indices
-        recipe_queue = deque(range(len(recipes)))
-        created_recipes = []
-        last_size = -1  # Tracks last known available count
-
-        # Continue while we keep finding new recipes
-        while len(available) > last_size:
-            last_size = len(available)
-            queue_size = len(recipe_queue)
-
-            # Process all recipes in current queue
-            while queue_size > 0:
-                queue_size -= 1
-                recipe_idx = recipe_queue.popleft()
-                if all(
-                    ingredient in available
-                    for ingredient in ingredients[recipe_idx]
-                ):
-                    # Recipe can be created - add to available items
-                    available.add(recipes[recipe_idx])
-                    created_recipes.append(recipes[recipe_idx])
-                else:
-                    recipe_queue.append(recipe_idx)
-
-        return created_recipes
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        # things that are dependent on it , the number of requirements for it
+        adj = defaultdict(lambda : [list(), 0])
+        for i in range(len(recipes)):
+            for supp in ingredients[i]:
+                adj[recipes[i]][1] += 1
+                adj[supp][0].append(recipes[i])
+        q = deque(supplies)
+        ans = []
+        while q:
+            curr = q.popleft()
+            for dependent in adj[curr][0]:
+                adj[dependent][1] -= 1
+                if adj[dependent][1] == 0:
+                    ans.append(dependent)
+                    q.append(dependent)
+        return ans
