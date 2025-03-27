04a2cc36ac8849b453123516a0228c0562039071
Henok <henitadesse97@gmail.com>
2025-03-27 17:55:07 +0300

    Time: 359 ms (58.11%) | Memory: 29.8 MB (15.85%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
@@ -1,16 +1,18 @@
 class Solution:
     def maximumCandies(self, candies: List[int], k: int) -> int:
-        total = sum(candies)
-        left , right = 0 , total // k
-        while left < right:
-            mid = (left + right) // 2 + 1
-            if self.all(candies, k , mid):
-                left = mid
+        left = 1
+        right = max(candies)
+        ans = 0
+        while left <= right:
+            mid = left + (right - left) // 2
+            if self.valid(mid, k, candies):
+                left = mid + 1
+                ans = mid
             else:
                 right = mid - 1
-        return left
-    def all(self, candies, k, mid):
+        return ans
+    def valid(self, guess, k, candies):
         counter = 0
-        for pile in candies:
-            counter += pile // mid
+        for cand in candies:
+            counter += cand // guess
         return counter >= k
