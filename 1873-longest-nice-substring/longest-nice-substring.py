2cf07ae76d29f61d8aca701665e96a27e377d92f
Henok <henitadesse97@gmail.com>
2025-03-28 18:36:09 +0300

    Time: 3 ms (73.28%) | Memory: 17.9 MB (40.88%) - LeetSync

--- 1873-longest-nice-substring/longest-nice-substring.py
+++ 1873-longest-nice-substring/longest-nice-substring.py
@@ -1,20 +1,22 @@
 class Solution:
     def longestNiceSubstring(self, s: str) -> str:
-        self.res = ""
-        self.cnt = float("-inf")
-        self.helper(s)
-        return self.res
+        # self.res = ""
+        # self.cnt = float("-inf")
+        ans = self.helper(s)
+        return ans
         
     def helper(self, s):
         if len(s) <= 1:
-            return
+            return ("")
         lett = set(s)
         for i in range(len(s)):
             if s[i].swapcase() not in lett:
-                self.helper(s[:i])
-                self.helper(s[i+1:])
-                return
-        if self.cnt < len(s):
-            self.res = s
-            self.cnt = max(self.cnt, len(s))
-        return
+                l = self.helper(s[:i])
+                r = self.helper(s[i+1:])
+                return (max(l , r , key=lambda x : len(x)))
+        return s
+
+        # if self.cnt < len(s):
+        #     self.res = s
+        #     self.cnt = max(self.cnt, len(s))
+        # return
