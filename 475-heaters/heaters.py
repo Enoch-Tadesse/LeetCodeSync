87474ffff7f75f698af728146a7e814a439f1ccf
Henok <henitadesse97@gmail.com>
2025-03-28 23:18:38 +0300

    Time: 806 ms (5.02%) | Memory: 20.7 MB (39.93%) - LeetSync

---
+++ 475-heaters/heaters.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        l , r = 0 , 10 ** 9
+        heaters.sort()
+        while l <= r:
+            mid = l + (r - l) // 2
+            if self.valid(mid, houses, heaters):
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l
+    def valid(self, guess, houses, heaters):
+        for num in houses:
+            l , r = num - guess , num + guess
+            l_exist = bisect_left(heaters, l)
+            r_exist = bisect_right(heaters, r)
+            # print(l_exist, r_exist)
+            if r_exist - l_exist == 0:
+                return False
+        return True
