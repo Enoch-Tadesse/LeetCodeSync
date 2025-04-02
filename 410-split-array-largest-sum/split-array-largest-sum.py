456b4b14a2e75731a9e9aafad9c744605a423e6e
Henok <henitadesse97@gmail.com>
2025-04-02 19:34:41 +0300

    Time: 4 ms (32.50%) | Memory: 17.7 MB (97.93%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.py
+++ 410-split-array-largest-sum/split-array-largest-sum.py
@@ -1,18 +1,24 @@
 class Solution:
-    def splitArray(self, nums: List[int], k: int) -> int:
-        l , r = max(nums) , sum(nums)
-        pre = nums.copy()
-        for i in range(1, len(pre)):
-            pre[i] += pre[i-1]
+    def splitArray(self, weights: List[int], k: int) -> int:
+        l = max(weights)
+        r = sum(weights)
         while l <= r:
             mid = l + (r - l) // 2
-            if k >= self.valid(mid, mid, pre, 0, len(pre)):
+            if self.canShip(weights, mid, k):
                 r = mid - 1
             else:
                 l = mid + 1
         return l
-    def valid(self, find, mid, nums, l , r):
-        if l >= r:
-            return 0
-        idx = bisect_right(nums, find, l , r)
-        return 1 + self.valid(mid + nums[idx-1], mid, nums, idx, r)
+        
+    def canShip(self, nums, weight, days):
+        curr = 0
+        counter = 1
+        for i in range(len(nums)):
+            if nums[i] > weight:
+                return False
+            if curr + nums[i] <= weight:
+                curr += nums[i]
+            else:
+                curr = nums[i]
+                counter += 1
+        return counter <= days
