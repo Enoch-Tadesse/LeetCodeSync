0a08e462ce4dfa4834518c8fe1fea1fd801e3ccf
Henok <henitadesse97@gmail.com>
2025-04-09 18:01:35 +0300

    Time: 447 ms (5.01%) | Memory: 19.1 MB (61.96%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = defaultdict(lambda : list())
+        for pre , nxt in prerequisites:
+            adj[pre].append(nxt)
+        self.path = set()
+        for i in range(numCourses):
+            self.visited = set()
+            if not self.traverse(i , adj):
+                return False
+            self.path.add(i)
+        return True
+
+    def traverse(self, curr, adj):
+        if curr in self.path:
+            return True
+        for pre in adj[curr]:
+            if pre in self.visited:
+                return False
+            self.visited.add(pre)
+            if not self.traverse(pre, adj):
+                return False
+            self.visited.discard(pre)
+        return True
