7a0d8bf760422d2bafa2751bc9bafb4c2b865c64
Henok <henitadesse97@gmail.com>
2025-09-03 18:21:55 +0300

    Time: 2473 ms (5.90%) | Memory: 85.4 MB (5.82%) - LeetSync

---
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
@@ -0,0 +1,48 @@
+class Trie:
+    def __init__(self):
+        self.chs = [None for _ in range(26)]
+        self.end = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = Trie()
+
+    def addWord(self, word: str) -> None:
+        root = self.root
+        for w in word:
+            idx = ord(w) - ord('a')
+            if not root.chs[idx]:
+                root.chs[idx] = Trie()
+            root = root.chs[idx]
+        root.end = True
+
+    def srh(self, word, i,  root):
+        if i == len(word) - 1 and word[i] == ".":
+            for r in root.chs:
+                if r and r.end:
+                    return True
+            return False
+        for j in range(i , len(word)):
+            w = word[j]
+            if w == ".":
+                for r in root.chs:
+                    if not r:
+                        continue
+                    if self.srh(word, j + 1, r):
+                        return True
+                return False
+            idx = ord(w) - ord('a')
+            if not root.chs[idx]:
+                return False
+            root = root.chs[idx]
+        return root.end
+
+    def search(self, word: str) -> bool:
+        return self.srh(word, 0, self.root)
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
