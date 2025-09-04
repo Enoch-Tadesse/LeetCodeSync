30b022f7b143b9a30ab13767d31baf907a0616d7
Henok <henitadesse97@gmail.com>
2025-09-04 11:19:29 +0300

    Time: 7 ms (4.59%) | Memory: 18.1 MB (8.07%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,11 +1,34 @@
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-        common_prefix = strs[0]
-        for s in strs:
-            while not s.startswith(common_prefix):
-                common_prefix = common_prefix[:-1]
-        return common_prefix
+class Trie:
+    def __init__ (self):
+        self.chs = [None for _ in range(26)]
+        self.count = 0
+    def insert(self, word):
+        root = self
+        for w in word:
+            idx = ord(w) - ord('a')
+            if not root.chs[idx]:
+                root.chs[idx] = Trie()
+            root = root.chs[idx]
+            root.count += 1
+    def get_common(self, size):
+        root = self
+        ans = []
+        carry = True
+        while carry:
+            for i in range(26):
+                r = root.chs[i]
+                if not r:
+                    continue
+                if r.count == size:
+                    ans.append(chr(ord('a') + i))
+                    root = root.chs[i]
+                    break
+            else:
+                carry = False
+        return "".join(ans)
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        trie = Trie()
+        for word in strs:
+            trie.insert(word)
+        return trie.get_common(len(strs))
