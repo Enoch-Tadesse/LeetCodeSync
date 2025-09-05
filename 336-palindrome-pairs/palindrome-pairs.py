85c40f552efbf81513c1dfeaac2a5a7f704054a7
Henok <henitadesse97@gmail.com>
2025-09-05 13:48:26 +0300

    Time: 1401 ms (72.13%) | Memory: 30.3 MB (19.23%) - LeetSync

--- 336-palindrome-pairs/palindrome-pairs.py
+++ 336-palindrome-pairs/palindrome-pairs.py
@@ -1,12 +1,12 @@
 class Solution:
     def palindromePairs(self, words: List[str]) -> List[List[int]]:
         seen = {w[::-1] : i for i , w in enumerate(words)}
-        ans = []
+        ans = set()
         for i , w in enumerate(words):
             for j in range(len(w) + 1):
                 pre , suf = w[:j] , w[j:]
                 if pre == pre[::-1] and suf in seen and seen[suf] != i:
-                    ans.append([seen[suf], i])
-                if j != len(w) and suf == suf[::-1] and pre in seen and seen[pre] != i:
-                    ans.append([i , seen[pre]])
-        return ans
+                    ans.add((seen[suf], i))
+                if suf == suf[::-1] and pre in seen and seen[pre] != i:
+                    ans.add((i , seen[pre]))
+        return [[l , r] for l , r in ans]
