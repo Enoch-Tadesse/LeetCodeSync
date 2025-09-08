350e6331693078d524cf6d3c10ed568eb9584efd
Henok <henitadesse97@gmail.com>
2025-09-08 11:56:40 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (12.69%) - LeetSync

---
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getNoZeroIntegers(self, n: int) -> List[int]:
+        def check(num):
+            while num > 0:
+                if num % 10 == 0:
+                    return False
+                num //= 10
+            return True
+        for i in range(1, n):
+            if check(i) and (check(n - i)):
+                return [i, n-i]
