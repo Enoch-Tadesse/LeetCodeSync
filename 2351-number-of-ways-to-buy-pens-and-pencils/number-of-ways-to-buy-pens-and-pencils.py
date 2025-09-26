21eb9a840bd73e90956fc88b1033bdbfd603f9eb
Henok <henitadesse97@gmail.com>
2025-09-26 11:07:42 +0300

    Time: 1278 ms (5.27%) | Memory: 60.8 MB (14.80%) - LeetSync

--- 2351-number-of-ways-to-buy-pens-and-pencils/number-of-ways-to-buy-pens-and-pencils.py
+++ 2351-number-of-ways-to-buy-pens-and-pencils/number-of-ways-to-buy-pens-and-pencils.py
@@ -1,8 +1,12 @@
 class Solution:
     def waysToBuyPensPencils(self, total: int, cost1: int, cost2: int) -> int:
-        ans = 0
-        for p in range(total // cost1 + 1):
-            left = total - cost1 * p
-            p2 = left // cost2
-            ans += p2 + 1
-        return ans
+        dp = [0] * (total + 1)
+        dp[0] = 1
+        
+        for i in range(cost1, total + 1):
+            dp[i] += dp[i - cost1]
+        
+        for i in range(cost2, total + 1):
+            dp[i] += dp[i - cost2]
+        
+        return sum(dp)
