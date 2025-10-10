5feef4b006d0ae2b3e7d03c88fe9bf68e371031e
Henok <henitadesse97@gmail.com>
2025-10-11 00:28:23 +0300

    Time: 362 ms (94.70%) | Memory: 19.7 MB (76.54%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,6 +1,7 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         dist = [float("inf")] * (n + 1)
+        visited = [0] * (n + 1)
         dist[0] = 0
         dist[k] = 0
         adj = defaultdict(list)
@@ -9,6 +10,9 @@ class Solution:
         h = [(0, k)]
         while h:
             d , key = heappop(h)
+            if visited[key] == 1:
+                continue
+            visited[key] = 1
             for nei , wei in adj[key]:
                 if dist[key] + wei < dist[nei]:
                     dist[nei] = dist[key] + wei
