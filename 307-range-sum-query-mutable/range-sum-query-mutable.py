8d7d767888641a2df41e946ac4354577df472770
Henok <henitadesse97@gmail.com>
2025-10-14 16:00:11 +0300

    Time: 405 ms (85.81%) | Memory: 36.6 MB (66.45%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.py
+++ 307-range-sum-query-mutable/range-sum-query-mutable.py
@@ -1,51 +1,44 @@
+class Fenwick:
+    def __init__(self, nums):
+        self.n = len(nums)
+        self.nums = [0] + nums
+        self.tree = [0] * (self.n + 1)
+        for i in range(1, self.n + 1):
+            self._insert(i, self.nums[i])
+
+    def _insert(self, idx, num):
+        while idx <= self.n:
+            self.tree[idx] += num
+            idx += idx & -idx
+
+    def update(self, idx, num):
+        idx += 1
+        diff = num - self.nums[idx]
+        self.nums[idx] = num
+        self._insert(idx, diff)
+
+    def _range(self, idx):
+        idx += 1
+        res = 0
+        while idx > 0:
+            res += self.tree[idx]
+            idx -= idx & -idx
+        return res
+        
+    def range_sum(self, l, r):
+        return self._range(r) - self._range(l - 1)
+
+
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.n = len(nums)
-        self.nums = nums
-        self.tree = [0] * (4 * self.n)
-        self.build(0, 0, self.n - 1)
-
-    def build(self, idx, l, r):
-        if l == r:
-            self.tree[idx] = self.nums[l]
-            return
-        mid = l + (r - l) // 2
-        lidx = idx * 2 + 1
-        ridx = idx * 2 + 2
-        self.build(lidx, l, mid)
-        self.build(ridx, mid + 1, r)
-        self.tree[idx] = self.tree[lidx] + self.tree[ridx]
-
-    def updateHelper(self, idx, l , r, pos, val):
-        if l == r:
-            self.tree[idx] = val
-            return
-        mid = l + (r - l) // 2
-        lidx = idx * 2 + 1
-        ridx = idx * 2 + 2
-        if pos <= mid:
-            self.updateHelper(lidx, l, mid, pos, val)
-        else:
-            self.updateHelper(ridx, mid + 1, r, pos, val)
-        self.tree[idx] = self.tree[lidx] + self.tree[ridx]
+        self.fenwick = Fenwick(nums)
 
     def update(self, index: int, val: int) -> None:
-        self.nums[index] = val
-        self.updateHelper(0, 0, self.n - 1, index, val)
-
-    def fetch(self, idx, l , r, ql, qr):
-        if l > qr or r < ql:
-            return 0
-        if ql <= l and qr >= r:
-            return self.tree[idx]
-        mid = l + (r - l) // 2
-        left = self.fetch(idx * 2 + 1, l, mid, ql, qr)
-        right = self.fetch(idx * 2 + 2, mid + 1, r, ql, qr)
-        return left + right
+        self.fenwick.update(index, val)
 
     def sumRange(self, left: int, right: int) -> int:
-        return self.fetch(0, 0, self.n - 1, left, right)
+        return self.fenwick.range_sum(left, right)
 
 
 # Your NumArray object will be instantiated and called as such:
