34552c1847910c63866e40bf5838b2ead8818217
Henok <henitadesse97@gmail.com>
2025-10-25 11:07:09 +0300

    Time: 545 ms (5.39%) | Memory: 83.3 MB (5.38%) - LeetSync

--- 1815-checking-existence-of-edge-length-limited-paths/checking-existence-of-edge-length-limited-paths.py
+++ 1815-checking-existence-of-edge-length-limited-paths/checking-existence-of-edge-length-limited-paths.py
@@ -1,13 +1,24 @@
 class Solution:
     def distanceLimitedPathsExist(self, n: int, edgeList: List[List[int]], queries: List[List[int]]) -> List[bool]:
         parent = {i : i for i in range(n)}
+        rank = {i : 0 for i in range(n)}
         def find(a):
             if parent[a] != a:
                 parent[a] = find(parent[a])
             return parent[a]
+
         def union(a, b):
             ua , ub = find(a) , find(b)
-            parent[ua] = ub
+            ra, rb = rank[a], rank[b]
+            if ra > rb:
+                parent[ub] = ua
+            elif ra < rb:
+                parent[ua] = ub
+            else:
+                parent[ua] = ub
+                rank[ub] += 1
+                
+
         qs = defaultdict(list)
         for index, (a , b , limit) in enumerate(queries):
             qs[limit].append((a, b, index))
