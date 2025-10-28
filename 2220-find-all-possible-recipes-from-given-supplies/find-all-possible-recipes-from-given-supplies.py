824d30f0beffd41c764dc56038bb969369aa8841
Henok <henitadesse97@gmail.com>
2025-10-28 09:47:45 +0300

    Time: 71 ms (64.83%) | Memory: 19.7 MB (55.11%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.py
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.py
@@ -1,18 +1,18 @@
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
-        # things that are dependent on it , the number of requirements for it
-        adj = defaultdict(lambda : [list(), 0])
-        for i in range(len(recipes)):
-            for supp in ingredients[i]:
-                adj[recipes[i]][1] += 1
-                adj[supp][0].append(recipes[i])
-        q = deque(supplies)
+        adj = defaultdict(lambda : [[], 0])
+        for i, r in enumerate(recipes):
+            adj[r][1] += len(ingredients[i])
+            for ing in ingredients[i]:
+                adj[ing][0].append(r)
         ans = []
+        q = deque(supplies)
         while q:
-            curr = q.popleft()
-            for dependent in adj[curr][0]:
-                adj[dependent][1] -= 1
-                if adj[dependent][1] == 0:
-                    ans.append(dependent)
-                    q.append(dependent)
+            for _ in range(len(q)):
+                curr = q.popleft()
+                for dep in adj[curr][0]:
+                    adj[dep][1] -= 1
+                    if adj[dep][1] == 0:
+                        ans.append(dep)
+                        q.append(dep)
         return ans
