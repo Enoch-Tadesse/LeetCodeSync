b142d472848848933fc040485f863ae082d7405e
Henok <henitadesse97@gmail.com>
2025-10-28 09:34:09 +0300

    Time: 41 ms (93.09%) | Memory: 17.9 MB (37.23%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.py
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.py
@@ -3,22 +3,15 @@ class Solution:
         # if they are equal, moving right and left are possible
         # if the right or left dominates just by one, only one is possible
         counter = 0
-        pre = []
+        total = sum(nums)
         curr = 0
-        for num in nums:
-            curr += num
-            pre.append(curr)
-        suf = [0] * len(nums)
-        curr = 0
-        for i in range(len(nums)-1, -1, -1):
-            curr += nums[i]
-            suf[i] = curr
-        # print(pre)
-        # print(suf)
         for i in range(len(nums)):
-            if nums[i] == 0:
-                if pre[i] == suf[i]:
-                    counter += 2
-                elif abs(pre[i] - suf[i]) == 1:
-                    counter += 1
+            if nums[i] != 0:
+                curr += nums[i]
+                continue
+            right = total - curr
+            if right == curr:
+                counter += 2
+            elif abs(right - curr) == 1:
+                counter += 1
         return counter
