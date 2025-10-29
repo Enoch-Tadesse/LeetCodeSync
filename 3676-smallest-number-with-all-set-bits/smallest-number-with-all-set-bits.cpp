b8f65a35140f9b435b659331b84c18dd277a1f5d
Henok <henitadesse97@gmail.com>
2025-10-29 06:51:04 +0300

    Time: 0 ms (100.00%) | Memory: 8.2 MB (60.80%) - LeetSync

---
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int smallestNumber(int n) {
+        int curr = 1;
+        while (curr < n) {
+            curr <<= 1;
+            curr |= 1;
+        }
+        return curr;
+    }
+};
