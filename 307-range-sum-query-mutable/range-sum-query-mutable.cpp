eeee4407dc49e27f996db16eedee78b3bd10f58e
Henok <henitadesse97@gmail.com>
2025-11-01 17:05:15 +0300

    Time: 72 ms (37.14%) | Memory: 184.1 MB (11.70%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -1,67 +1,62 @@
-#include <climits>
-#include <vector>
-using namespace std;
-
 typedef vector<int> vi;
+
 class SegmentTree {
     private:
-        vi nums, st;
+        vi nums, sg;
         int n;
-        int left(int x) { return (x << 1) + 1; }
-        int right(int x) { return (x << 1) + 2; }
-        void build(int p, int l, int r) {
+        int left(int x) {return (x << 1) + 1;}
+        int right(int x) {return (x << 1) + 2;}
+        void build(int p, int l, int r){
             if (l == r) {
-                st[p] = nums[l];
+                sg[p] = nums[l];
                 return;
             }
-
             int mid = l + (r - l) / 2;
-            build(left(p), l, mid);
-            build(right(p), mid + 1, r);
-            st[p] = st[left(p)] + st[right(p)];
+            int le = left(p), ri = right(p);
+            build(le, l, mid); build(ri, mid + 1, r);
+            sg[p] = sg[le] + sg[ri];
         }
-
-        int queryHelper(int p, int l, int r, int ql, int qr) {
-            if (qr < l || ql > r) return 0;
-            if (ql <= l && r <= qr) return st[p];
+        int queryHelper(int p, int l, int r, int L, int R) {
+            if (l > R || r < L) return 0;
+            if (L <= l && r <= R) return sg[p];
+            int le = left(p), ri = right(p);
             int mid = l + (r - l) / 2;
-            int s1 = queryHelper(left(p), l, mid, ql, qr);
-            int s2 = queryHelper(right(p), mid + 1, r, ql, qr);
-            return s1 + s2;
+            int s1 = queryHelper(le, l, mid, L, R);
+            int s2 = queryHelper(ri, mid + 1, r, L, R);
+            return s1 + s2;            
         }
-
         void updateHelper(int p, int idx, int val, int l, int r) {
             if (l == r) {
-                st[p] = val;
+                sg[p] = val;
                 return;
             }
             int mid = l + (r - l) / 2;
-            if (idx <= mid)
-                updateHelper(left(p), idx, val, l, mid);
-            else
-                updateHelper(right(p), idx, val, mid + 1, r);
-            st[p] = st[left(p)] + st[right(p)];
+            if (idx <= mid) updateHelper(left(p), idx, val, l, mid);
+            else updateHelper(right(p), idx, val, mid + 1, r);
+            sg[p] = sg[left(p)] + sg[right(p)];
         }
-
     public:
         SegmentTree(vi nums) {
             this->nums = nums;
             n = nums.size();
-            st.assign(4 * n, 0);
+            sg.assign(4 *n, 0);
             build(0, 0, n - 1);
         }
 
-        int query(int l, int r) { return queryHelper(0, 0, n - 1, l, r); }
-        void update(int idx, int val) {
-            updateHelper(0, idx, val, 0, n - 1);
-            nums[idx] = val;
+        int query(int left, int right) {
+            return queryHelper(0, 0, n - 1, left, right);
         }
+
+        void update(int index, int val) {
+            updateHelper(0, index, val, 0, n - 1);
+            nums[index] = val;
+        }
+
 };
 
-
 class NumArray {
-    SegmentTree *sg;
 public:
+    SegmentTree *sg;
     NumArray(vector<int>& nums) {
         sg = new SegmentTree(nums);
     }
