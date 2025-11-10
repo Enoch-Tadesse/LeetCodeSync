7916652118433ecedce46ee80a8ba4dc88e2e48c
Henok <henitadesse97@gmail.com>
2025-11-10 21:24:25 +0300

    Time: 3 ms (31.07%) | Memory: 17.8 MB (71.71%) - LeetSync

--- 1651-shuffle-string/shuffle-string.py
+++ 1651-shuffle-string/shuffle-string.py
@@ -1,6 +1,12 @@
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
-        ans = [""] * len(s)
-        for i in range(len(indices)):
-            ans[indices[i]] = s[i]
+        # intial let;s create same size of s, for answer
+        n = len(s)
+        ans = [""] * n
+        
+        for i in range(n):
+            character = s[i]
+            target_index = indices[i]
+            ans[target_index] = character
+
         return "".join(ans)
