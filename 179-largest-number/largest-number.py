3b00756ac563a592dc7a9c3ecc31312e972e113f
Henok <henitadesse97@gmail.com>
2025-11-20 16:00:14 +0300

    Time: 3 ms (52.56%) | Memory: 17.7 MB (87.44%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -1,9 +1,10 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         for i in range(len(nums)):
-            for j in range(len(nums) - i - 1):
-                cand1 = int(str(nums[j]) + str(nums[j + 1]))
-                cand2 = int(str(nums[j + 1]) + str(nums[j]))
-                if cand2 > cand1:
-                    nums[j] , nums[ j + 1] = nums[j + 1], nums[j]
-        return str(int("".join(map(str, nums))))
+            nums[i] = str(nums[i])
+        def compare(n1, n2):
+            if n1 + n2 > n2 + n1:
+                return -1 # n1 comes before n2
+            return 1 # n2 comes before n1
+        nums.sort(key=cmp_to_key(compare))
+        return str(int("".join(nums)))
