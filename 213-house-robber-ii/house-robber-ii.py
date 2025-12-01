909c3defd048c21aee24c031a06260951c9fef30
Henok <henitadesse97@gmail.com>
2025-12-01 13:29:43 +0300

    Time: 1 ms (16.14%) | Memory: 17.8 MB (59.42%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -2,12 +2,10 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         if len(nums) <= 1:
             return sum(nums)
-        ans = 0
-        for i in range(len(nums)):
+        def calculate(arr):
             r, nr = 0 , 0
-            for j in range(i + 1, len(nums) + i):
-                j = j % len(nums)
-                num = nums[j]
-                r, nr = nr + num, max(r, nr)
-            ans = max(ans, r, nr)
-        return ans
+            for num in arr:
+                r, nr = num + nr, max(r, nr)
+            return max(r, nr)
+        return max(calculate(nums[1:]), calculate(nums[:-1]))
+    
