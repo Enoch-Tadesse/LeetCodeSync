2fb06c9fcbf2ca4e8cff51f7357a14689eff5a72
Henok <henitadesse97@gmail.com>
2026-02-04 10:46:28 +0300

    Time: 417 ms (57.06%) | Memory: 62.6 MB (70.59%) - LeetSync

--- 1281-can-make-palindrome-from-substring/can-make-palindrome-from-substring.py
+++ 1281-can-make-palindrome-from-substring/can-make-palindrome-from-substring.py
@@ -1,56 +1,19 @@
 class Solution:
     def canMakePaliQueries(self, s: str, queries: List[List[int]]) -> List[bool]:
         n = len(s)
-        size = int(n ** 0.5) + 1
-        groups = [[] for _ in range(size)]
+        pre = [0]
 
-        for i, enum in enumerate(queries):
-            l, r, k = enum
-            groups[l // size].append((l, r, k, i))
-        
-        ans = 0
-        q = len(queries)
-        output = [False] * q
-        L, R = 0, -1
-
-        cnts = defaultdict(int)
-        def add(index):
-            nonlocal ans
-            ele = s[index]
-            cnts[ele] += 1
-            if cnts[ele] & 1: ans += 1
-            else: ans -= 1
-        def remove(index):
-            nonlocal ans
-            ele = s[index]
-            cnts[ele] -= 1
-            if cnts[ele] & 1: ans += 1
-            else: ans -= 1
-
-
-        for g in groups:
-            if len(g) == 0:
-                continue
-            g.sort(key=lambda x : (x[1], x[0]))
-
-            for l , r, k, i in g:
-                while R < r:
-                    R += 1
-                    add(R)
-                while R > r:
-                    remove(R)
-                    R -= 1
-                while L < l:
-                    remove(L)
-                    L += 1
-                while L > l:
-                    L -= 1
-                    add(L)
-                length = r - l + 1
-                to = 0
-                if length & 1 == 0:
-                    to = ans // 2
-                else:
-                    to = (ans - 1) // 2
-                output[i] = (to <= k)
+        for c in s:
+            idx = ord(c) - ord('a')
+            pre.append(pre[-1] ^ (1 << idx))
+        output = []
+        for l, r, k in queries:
+            odds = 0
+            prev = pre[l]
+            now = pre[r + 1]
+            for i in range(26):
+                odds += (prev >> i) & 1 != (now >> i) & 1
+            
+            
+            output.append(odds//2 <= k)
         return output
