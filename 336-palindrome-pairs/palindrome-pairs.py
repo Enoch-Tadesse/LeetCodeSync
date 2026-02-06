c35d5bb621723d292fe651dd27afa7942a9319c2
Henok <henitadesse97@gmail.com>
2026-02-06 11:56:41 +0300

    Time: 1752 ms (64.01%) | Memory: 30.3 MB (35.59%) - LeetSync

--- 336-palindrome-pairs/palindrome-pairs.py
+++ 336-palindrome-pairs/palindrome-pairs.py
@@ -1,18 +1,28 @@
 class Solution:
     def palindromePairs(self, words: List[str]) -> List[List[int]]:
-        reverse = {word[::-1] : i for i, word in enumerate(words)}
+        
+        rev = {word : i for i, word in enumerate(words)}
 
         ans = set()
 
-        for i, word in enumerate(words):
-            for j in range(len(word) + 1):
-                pre , suf = word[:j], word[j:]
+        for curr, word in enumerate(words):
+            n = len(word)
 
-                if pre == pre[::-1] and suf in reverse and reverse[suf] != i:
-                    ans.add((reverse[suf], i))
+            for i in range(n + 1):
+                left = word[:i]
+                right = word[i:] if i < n else ""
 
-                if suf == suf[::-1] and pre in reverse and reverse[pre] != i:
-                    ans.add((i, reverse[pre]))
 
+                if left == left[::-1] and right[::-1] in rev:
+                    new_idx = rev[right[::-1]]
+                    if new_idx != curr:
+                        ans.add((new_idx,curr))
+
+                if right == right[::-1] and left[::-1] in rev:
+                    new_idx = rev[left[::-1]]
+                    if curr != new_idx:
+                        ans.add((curr, new_idx))          
 
         return list(ans)
+
+            
