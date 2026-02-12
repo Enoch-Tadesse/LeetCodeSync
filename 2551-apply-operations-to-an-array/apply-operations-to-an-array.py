95d9ea8c9d31089c9c3f4d4ee56fb923d4c4f20e
Henok <henitadesse97@gmail.com>
2026-02-12 15:52:40 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (59.42%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -1,18 +1,19 @@
-class Solution(object):
-    def applyOperations(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        result = []
-        for i in range(len(nums)-1):
-            if nums[i] == nums[i+1]:
-                nums[i]*=2
-                nums[i+1] = 0
-        ptr = 0
-        for i in range(len(nums)):
-            if nums[i]!=0:
-                nums[i] , nums[ptr] = nums[ptr] , nums[i]
-                ptr+=1
-        return nums
-        
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        # 
+        # [1,2,4,0,0,0]
+
+        # iternating over nums until len(nums) - 1
+
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                nums[i] *= 2
+                nums[i + 1] = 0
+        # [1,4,0,2,0,0]
+        ans = []
+        for num in nums:
+            if num != 0:
+                ans.append(num)
+        zeros = len(nums) - len(ans)
+        ans.extend([0] * zeros)
+        return ans
