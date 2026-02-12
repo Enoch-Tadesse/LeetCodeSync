a6bb6f29e88ced87b9ea66ab05e8ace16d17b9e2
Henok <henitadesse97@gmail.com>
2026-02-12 16:55:33 +0300

    Time: 134 ms (76.13%) | Memory: 20 MB (62.31%) - LeetSync

--- 661-image-smoother/image-smoother.py
+++ 661-image-smoother/image-smoother.py
@@ -1,16 +1,21 @@
 class Solution:
     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
-        rows, cols = len(img) , len(img[0])
+        # m * n * 9
 
-        ans = [[0] * cols for _ in range(rows)]
+        rows, cols = len(img), len(img[0])
+
+        out = [[0] * cols for _ in range(rows)]
 
         for r in range(rows):
             for c in range(cols):
-                _sum = 0
                 _count = 0
-                for nr in range(max(0, r - 1), min(r + 2, rows)):
-                    for nc in range(max(0, c - 1), min(c + 2, cols)):
-                        _sum += img[nr][nc]
+                _sum = 0
+                # process the _count and _sum
+
+                for new_row in range(max(0, r - 1), min(rows, r + 2)):
+                    for new_col in range(max(0, c - 1), min(cols, c + 2)):
                         _count += 1
-                ans[r][c] = _sum // _count
-        return ans
+                        _sum += img[new_row][new_col]
+
+                out[r][c] = _sum // _count
+        return out
