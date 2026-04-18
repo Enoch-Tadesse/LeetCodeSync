8653247e120f2d00586697afb6355c4f398e0f44
Henok <henitadesse97@gmail.com>
2026-04-18 19:36:06 +0300

    Time: 20 ms (5.02%) | Memory: 19.2 MB (71.37%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -1,10 +1,22 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        for i in range(len(nums)):
-            nums[i] = str(nums[i])
-        def compare(n1, n2):
-            if n1 + n2 > n2 + n1:
-                return -1 # n1 comes before n2
-            return 1 # n2 comes before n1
-        nums.sort(key=cmp_to_key(compare))
-        return str(int("".join(nums)))
+        [3,30,34,5,9]
+        
+        [9, 5,  34,  3, 30]
+
+        nums = [str(num) for num in nums]
+
+        def compare(num1, num2):
+            pos1, pos2 = num1 + num2, num2 + num1
+            if int(pos1) > int(pos2):
+                return True
+            return False
+        
+
+        for i in range(len(nums) - 1, -1, -1):
+            for j in range(i):
+                if not compare(nums[j], nums[j + 1]):
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+        ans = "".join(nums)
+        return str(int(ans))
+        
