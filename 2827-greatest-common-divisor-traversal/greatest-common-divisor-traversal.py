a8f3b169e4dfc57d806c6b7fdd3b6e3df2d56b2b
Henok <henitadesse97@gmail.com>
2026-05-29 10:44:52 +0300

    Time: 634 ms (65.54%) | Memory: 76.3 MB (5.65%) - LeetSync

--- 2827-greatest-common-divisor-traversal/greatest-common-divisor-traversal.py
+++ 2827-greatest-common-divisor-traversal/greatest-common-divisor-traversal.py
@@ -1,22 +1,16 @@
+_max = 10 ** 5 + 7
+pre = [0] * _max
+for i in range(2, _max):
+    if pre[i] == 0:
+        for j in range(i, _max, i):
+            if pre[j] == 0:
+                pre[j] = i
+
+
 class Solution:
     def canTraverseAllPairs(self, nums: List[int]) -> bool:
         
         fac = defaultdict(set)
-        def factor(num, idx):
-            i = 2
-            while i * i <= num:
-                if num % i != 0:
-                    i += 1
-                    continue
-                while num % i == 0:
-                    num //= i
-                fac[i].add(idx)
-                i += 1
-            if num > 1:
-                fac[num].add(idx)
-        for  i in range(len(nums)):
-            factor(nums[i], i)
-        print(fac)
         par = {i : i for i in range(len(nums))}
         def find(x):
             if x != par[x]:
@@ -25,6 +19,15 @@ class Solution:
         def union(x, y):
             px, py = find(x), find(y)
             par[px] = py
+
+        def factor(x, i):
+            while x > 1:
+                fac[pre[x]].add(i)
+                x //= pre[x]
+
+        for  i in range(len(nums)):
+            factor(nums[i], i)
+        
         for v in fac.values():
             v = list(v)
             for i in range(1, len(v)):
