60b1b7680eb1e80f9ab87f3842bf8c9b8b0541d0
Henok <henitadesse97@gmail.com>
2026-06-11 08:13:52 +0300

    Time: 458 ms (21.50%) | Memory: 135 MB (7.53%) - LeetSync

---
+++ 3844-number-of-ways-to-assign-edge-weights-i/number-of-ways-to-assign-edge-weights-i.py
@@ -0,0 +1,21 @@
+class Solution:
+    def assignEdgeWeights(self, edges: List[List[int]]) -> int:
+        adj = defaultdict(list)
+        for a, b in edges:
+            adj[a].append(b)
+            adj[b].append(a)
+
+        n = len(edges)
+        vis = set([1])
+        
+        def dfs(node):
+            if not node:
+                return 0
+            depth = 0
+            for nei in adj[node]:
+                if nei in vis: continue
+                vis.add(nei)
+                depth = max(depth, dfs(nei) + 1)
+            return depth
+
+        return pow(2, dfs(1) - 1, 10 ** 9 + 7) % (10 ** 9 + 7)
