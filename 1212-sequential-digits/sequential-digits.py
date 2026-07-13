94681d1befd651153c715d4bcaba2c4bd0cf7dfa
Henok <henitadesse97@gmail.com>
2026-07-13 21:15:16 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (64.31%) - LeetSync

--- 1212-sequential-digits/sequential-digits.py
+++ 1212-sequential-digits/sequential-digits.py
@@ -1,17 +1,10 @@
 class Solution:
     def sequentialDigits(self, low: int, high: int) -> List[int]:
         ans = []
-        def back(num):
-            nonlocal ans
-            if num > high:
-                return
-            if num >= low:
-                ans.append(num)
-            last = num % 10
-            if last == 9:
-                return
-            nxt = num * 10 + (last + 1)
-            back(nxt)
         for i in range(1, 10):
-            back(i)
+            cur = i
+            for j in range(i + 1, 10):
+                cur = (cur * 10) + j
+                if low <= cur <= high:
+                    ans.append(cur)
         return sorted(ans)
