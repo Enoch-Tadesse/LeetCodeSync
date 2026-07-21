e7741901261061fdada2906ae89bc7a49fe8e61f
Henok <henitadesse97@gmail.com>
2026-07-21 07:38:41 +0300

    Time: 1755 ms (5.61%) | Memory: 39.7 MB (5.61%) - LeetSync

--- 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.py
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.py
@@ -2,7 +2,9 @@ class Solution:
     def maxActiveSectionsAfterTrade(self, s: str) -> int:
         cand = []
         l = 0
+        pre = [0]
         for r in range(len(s)):
+            pre.append(pre[-1] + (s[r] == "1"))
             if s[r] == s[l]:
                 continue
             fac = -1 if s[l] == "1" else 1
@@ -10,10 +12,6 @@ class Solution:
             l = r
         fac = -1 if s[l] == "1" else 1
         cand.append((fac * (len(s) - l), l, len(s) - 1))
-
-        pre = [0]
-        for i in range(len(s)):
-            pre.append(pre[-1] + (s[i] == "1"))
 
         ans = 0
         for i in range(1, len(cand) - 1):
@@ -27,4 +25,5 @@ class Solution:
 
         if ans == 0:
             ans = s.count("1")
+
         return ans
